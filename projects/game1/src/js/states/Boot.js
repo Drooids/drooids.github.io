a376257ec21f6764a391edbddda752d2294c639c
Dren Kajmakchi <dk18184@gmail.com>
2017-08-16 10:27:16 +0200

    wip: mobile renderer

--- projects/game1/src/js/states/Boot.js
+++ projects/game1/src/js/states/Boot.js
@@ -5,6 +5,8 @@
     }
 
     Boot.prototype.preload = function() {
+        
+        var Constants = {}; // todo: Edit later on...
 
         // Load animations before game start
         game.load.json('preload-loading-icon',
@@ -19,17 +21,60 @@
             game.scale.pageAlignHorizontally = true;
             game.scale.pageAlignVertically = true;
         } else {
-            game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
-            game.scale.minWidth = 480;
-            game.scale.minHeight = 260;
-            game.scale.maxWidth = 640;
-            game.scale.maxHeight = 480;
+            game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+            game.scale.fullScreenScaleMode = Phaser.ScaleManager.USER_SCALE;
+            
+            var manager = game.scale;
+            
+            var width = window.innerWidth;
+            var height = window.innerHeight;
+            
+            Boot.setScaling(manager.game);
+            
+            var usedWidth = g_.screen.width * Constants.GAME_SCALE;
+            var usedHeight = g_.screen.width * Constants.GAME_SCALE;
+            
+            var scaleFactor = 1;
+            
+             //So first we check if the game is beeing played in landscape
+            if (width > height) {
+                scaleFactor /= height / usedHeight;
+            } else {
+                scaleFactor /= height / usedWidth;
+            }
+
+            Constants.CALCULATED_WIDTH = Math.ceil(width * scaleFactor);
+            Constants.CALCULATED_HEIGHT = Math.ceil(height * scaleFactor);
+
+            manager.setGameSize(Constants.CALCULATED_WIDTH, Constants.CALCULATED_HEIGHT);
+            manager.setUserScale(1 / scaleFactor, 1 / scaleFactor);
+        
+            // game.scale.scaleMode = Phaser.ScaleManager.RESIZE;
+            // game.scale.minWidth = 480;
+            // game.scale.minHeight = 260;
+            // game.scale.maxWidth = 640;
+            // game.scale.maxHeight = 480;
             // game.scale.forceOrientation(true);
             // game.scale.pageAlignHorizontally = true;
-            game.scale.refresh();
+            // game.scale.refresh();
         }
         
-        alert("version: 0.2");
+        alert("version: 0.3");
+    };
+    
+    Boot.prototype.setScaling = function(game) {
+        //Check if the device is in portrait mode, and if so, override the width with the innerHeight.
+        //We want to determine the scaling based on the the biggest side.
+        var width = window.innerWidth > window.innerHeight ? window.innerWidth : window.innerHeight;
+        width *= game.device.pixelRatio;
+
+        if (width < 650) {
+            Constants.GAME_SCALE = 0.5;
+        } else if (width > 1050) {
+            Constants.GAME_SCALE = 1;
+        } else {
+            Constants.GAME_SCALE = 0.75;
+        }
     };
 
     Boot.prototype.create = function() {
